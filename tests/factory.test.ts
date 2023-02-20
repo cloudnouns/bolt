5c10c069a692790affc7219d46c5d9d9d1f8e450
Badu Blanc <badu@hey.com>
2023-02-20 04:46:16 -0600

    add basic test

--- tests/factory.test.ts
+++ tests/factory.test.ts
@@ -1,7 +1,22 @@
 import { describe, it, expect } from 'vitest';
+import { Factory } from '../src/package/factory';
+import NounsData from '../src/data/nouns';
 
-describe('test', () => {
-	it('should', () => {
-		expect(5).toEqual(4);
+const factory = new Factory(NounsData);
+
+describe('Factory', () => {
+	it('should do something', () => {
+		const noun = factory.createItem({ glasses: 'fullblack' });
+		expect(noun.seed.glasses).toEqual(7);
+
+		const noun2 = factory.createItemFromSeed({
+			background: -99,
+			body: -99,
+			accessory: -99,
+			head: -99,
+			glasses: -99,
+		});
+		expect(noun2.background).toEqual('transparent');
+		expect(noun2.glasses).toEqual('intentionally-blank');
 	});
 });
